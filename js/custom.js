21dcf7adae5fb3d737161bc2b4a8d4e7f7651fb0
wwwoojung <50447560+wwwoojung@users.noreply.github.com>
2023-12-15 14:42:45 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,8 +2,43 @@ $(function () {
     $('.main_visual .visual_text').removeClass('on');
     $('.main_visual .visual_text').addClass('on');
 
+    $(window).on('scroll', function () {
+        const sct = $(window).scrollTop();
+
+        if (sct > 0) {
+            $('.header_wrap').addClass('on')
+        } else {
+            $('.header_wrap').removeClass('on')
+        }
+    })
+
     $('.main_category .category_itm').slick({
         slidesToShow: 4,
         arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+    })
+
+    $('.main_category .category_name a').on('click', function (e) {
+        e.preventDefault();
+
+        let idx = $(this).parent().index();
+
+        $('.main_category .category_name li').removeClass('on');
+        $(this).parent().addClass('on');
+
+        $('.main_category .category_itm').slick('slickGoTo', idx);
+    })
+
+    $('.main_event .event_con .material-symbols-outlined').on('click', function () {
+        $(this).toggleClass('on');
+    })
+
+    $('#footer .footer_top .more_btn').on('click', function () {
+        $('#footer .footer_top .right_con .country_box').addClass('on');
+    })
+
+    $('#footer .footer_top .right_con .country_box .close').on('click', function () {
+        $('#footer .footer_top .right_con .country_box').removeClass('on');
     })
 })
